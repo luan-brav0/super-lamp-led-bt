b5d33b5b30df2a02e446765de4a064fa35550d5f
luan-brav0 <luan.melo.bravo10@gmail.com>
2024-09-23 16:38:21 -0300

    file reading for separate html file added | comments removed

--- super_lamp.ino
+++ super_lamp.ino
@@ -6,6 +6,7 @@
 
 // Load Wi-Fi library
 #include <WiFi.h>
+#include <LittleFS.h>
 
 // Red, green, and blue pins for PWM control
 #define RED_PIN 25
@@ -74,15 +75,6 @@ void setup() {
     }
     Serial.println("\nConnected to WiFi!\n");
 
-    // configure LED PWM functionalitites
-    /*
-    * ledcSetup(red_channel, freqColor, resolution);
-    * ledcSetup(green_channel, freqColor, resolution);
-    * ledcSetup(blue_channel, freqColor, resolution);
-    * // Configure BUZZER PWMs
-    * ledcSetup(channel, freqMelody, resolution);
-    */
-    // attach the channel to the GPIO to be controlled
     ledcAttach(MELODY_PIN, freqMelody, resolution);
     ledcAttach(RED_PIN, freqColor, resolution);
     ledcAttach(GREEN_PIN, freqColor, resolution);
@@ -91,44 +83,45 @@ void setup() {
     pinMode(BUTTON_ON, INPUT);
     server.begin();
     button_prev = HIGH;
+
+    if (!LittleFS.begin())
+        Serial.println("LittleFS initialization failed!");
+    else
+        Serial.println("LittleFS initialization done.");
 }
 
 void loop() {
-    WiFiClient client = server.available();   // Listen for incoming clients
+    WiFiClient client = server.available();
 
-    if (client) {                             // If a new client connects,
+    if (client) {
         current_time = millis();
         previous_time = current_time;
-        Serial.println("New Client.");          // print a message out in the serial port
-        String currentLine = "";                // make a String to hold incoming data from the client
-        while (client.connected() && current_time - previous_time <= timeout_times) {            // loop while the client's connected
+        Serial.println("New Client.");
+        String currentLine = "";
+        while (client.connected() && current_time - previous_time <= timeout_times) {
             current_time = millis();
-            if (client.available()) {             // if there's bytes to read from the client,
-                char c = client.read();             // read a byte, then
-                Serial.write(c);                    // print it out the serial monitor
+            if (client.available()) {
+                char c = client.read();
+                Serial.write(c);
                 header += c;
-                if (c == '\n') {                    // if the byte is a newline character
-                    // if the current line is blank, you got two newline characters in a row.
-                    // that's the end of the client HTTP request, so send a response:
+                if (c == '\n') {
                     if (currentLine.length() == 0) {
-                        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-                        // and a content-type so the client knows what's coming, then a blank line:
                         client.println("HTTP/1.1 200 OK");
                         client.println("Content-type:text/html");
                         client.println("Connection: close");
                         client.println();
-                        // Display the HTML web page
-                        client.println("<!DOCTYPE html><html>");
-                        client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-                        client.println("<link rel=\"icon\" href=\"data:,\">");
-                        client.println("<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\">");
-                        client.println("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jscolor/2.0.4/jscolor.min.js\"></script>");
-                        client.println("</head><body><div class=\"container\"><div class=\"row\"><h1>ESP Color Picker</h1></div>");
-                        client.println("<a class=\"btn btn-primary btn-lg\" href=\"#\" id=\"change_color\" role=\"button\">Change Color</a> ");
-                        client.println("<input class=\"jscolor {onFineChange:'update(this)'}\" id=\"rgb\"></div>");
-                        client.println("<script>function update(picker) {document.getElementById('rgb').innerHTML = Math.round(picker.rgb[0]) + ', ' +  Math.round(picker.rgb[1]) + ', ' + Math.round(picker.rgb[2]);");
-                        client.println("document.getElementById(\"change_color\").href=\"?r\" + Math.round(picker.rgb[0]) + \"g\" +  Math.round(picker.rgb[1]) + \"b\" + Math.round(picker.rgb[2]) + \"&\";}</script></body></html>");
-                        // The HTTP response ends with another blank line
+
+                        // read index.html
+                        File file = LittleFS.open("/index.html", "r");
+                        if (!file) {
+                            Serial.println("Failed to open file for reading");
+                            return;
+                        }
+                        while (file.available()) {
+                            client.write(file.read());
+                        }
+                        file.close();
+
                         client.println();
                         // Request sample: /?r201g32b255&
                         // Red = 201 | Green = 32 | Blue = 255
@@ -155,51 +148,49 @@ void loop() {
                                 ledcWrite(green_channel, green_string.toInt());
                                 ledcWrite(blue_channel, blue_string.toInt());
                              }
+                            break;
+                        } else {
+                            currentLine = "";
                         }
-                        // Break out of the while loop
-                        break;
-                    } else { // if you got a newline, then clear currentLine
-                        currentLine = "";
+                    } else if (c != '\r') {
+                        currentLine += c;
                     }
-                } else if (c != '\r') {  // if you got anything else but a carriage return character,
-                    currentLine += c;      // add it to the end of the currentLine
                 }
             }
         }
-        // Clear the header variable
-        header = "";
-        // Close the connection
-        client.stop();
-        Serial.println("Client disconnected.");
-        Serial.println("");
     }
+    header = "";
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+}
 
-    button = digitalRead(BUTTON_ON);
-    if (button == LOW) {
-        if (button_prev == HIGH) {
-            button_prev = LOW;
-            lamp_on = 1;
-            red_string = "255";
-            green_string = "255";
-            blue_string = "255";
-            ledcWrite(red_channel, red_string.toInt());
-            ledcWrite(green_channel, green_string.toInt());
-            ledcWrite(blue_channel, blue_string.toInt());
-            singTheme();
+button = digitalRead(BUTTON_ON);
+if (button == LOW) {
+    if (button_prev == HIGH) {
+        button_prev = LOW;
+        lamp_on = 1;
+        red_string = "255";
+        green_string = "255";
+        blue_string = "255";
+        ledcWrite(red_channel, red_string.toInt());
+        ledcWrite(green_channel, green_string.toInt());
+        ledcWrite(blue_channel, blue_string.toInt());
+        singTheme();
 
-        } else {
-            button_prev = HIGH;
-            delay(400);
-            lamp_on = 0;
-            red_string = "0";
-            green_string = "0";
-            blue_string = "0";
-            ledcWrite(red_channel, red_string.toInt());
-            ledcWrite(green_channel, green_string.toInt());
-            ledcWrite(blue_channel, blue_string.toInt());
-            singPipe();
-        }
+    } else {
+        button_prev = HIGH;
+        delay(400);
+        lamp_on = 0;
+        red_string = "0";
+        green_string = "0";
+        blue_string = "0";
+        ledcWrite(red_channel, red_string.toInt());
+        ledcWrite(green_channel, green_string.toInt());
+        ledcWrite(blue_channel, blue_string.toInt());
+        singPipe();
     }
+}
 }
 
 void singPipe(void) {
